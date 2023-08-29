a0286c9d5ec75ef700044f013beb0452e84be0f0
Alexey Zorkaltsev <AZorkaltsev@Yandex-Team.ru>
2023-08-29 19:20:49 +0300

    fix: fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { YCServerlessLogger } from './utils/yc-serverless-logger';
 
 import * as dotenv from 'dotenv';
+import { start } from 'repl';
 
 dotenv.config();
 
@@ -8,27 +9,43 @@ const logger = new YCServerlessLogger({ showLevel: true, showTimestamp: true });
 
 const started: number = Date.now();
 
+class Interval {
+  constructor(readonly periodSecs: number) {}
+
+  toString() {
+    const h = Math.trunc(this.periodSecs / 60 / 60);
+    const m = Math.trunc(this.periodSecs / 60) - h * 60;
+    const s = Math.trunc(this.periodSecs % 60);
+    return `${h}h ${m}m ${s}s`;
+  }
+}
+
 export const handler = async (
   event: object,
   context: { [key in string]: unknown },
 ) => {
   try {
+    if ((context.token as any) === undefined) {
+      throw new Error('SA is not specified for serverless function');
+    }
+
     // TODO: Add check that SA was provided
 
     logger.info(event, 'event');
     logger.info(context, 'context');
 
-    const h = Math.trunc((context.token as any).expires_in / 60 / 60);
-    const m = Math.trunc((context.token as any).expires_in / 60) - h * 60;
-
     logger.warn(
-      'Token TTL: %d h %d m; Working from start %d h',
-      h,
-      m,
-      (Date.now() - started) / 1000 / 60 / 60,
+      'Token TTL: %s; Time from build: %s',
+      new Interval((context.token as any).expires_in),
+      new Interval((Date.now() - started) / 1000),
     );
   } catch (error) {
     logger.error(error, (error as Error).message);
     throw error;
   }
+
+  return {
+    statusCode: 200,
+    body: {},
+  };
 };
